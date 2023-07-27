dfbd7c0d0d6e7473ebd7e8e0fcc0bd48656b3dc0
TPNYC\azhu <azhu@transperfect.com>
2023-07-27 13:18:44 -0400

    fix createddate & updateddate not updating

--- BankingWebApi/BankingWebApi/Context/AccountsDbContext.cs
+++ BankingWebApi/BankingWebApi/Context/AccountsDbContext.cs
@@ -7,8 +7,52 @@ namespace BankingWebApi.Context
     {
         public AccountsDbContext(DbContextOptions options) : base(options)
         {
+            SavingChanges += AccountsContext_SavingChanges;
         }
 
         public DbSet<Account> Accounts { get; set; }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                return base.SaveChangesAsync(cancellationToken);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+        }
+
+        private void AccountsContext_SavingChanges(object? sender, SavingChangesEventArgs e)
+        {
+            var entityStates = new EntityState[]
+            {
+            EntityState.Added,
+            EntityState.Modified,
+            EntityState.Deleted,
+            };
+
+            foreach (var changedEntity in ChangeTracker
+                        .Entries()
+                        .Where(e => e.Entity is Entity && entityStates.Contains(e.State)))
+            {
+                var entity = (Entity)changedEntity.Entity;
+                switch (changedEntity.State)
+                {
+                    case EntityState.Added:
+                        {
+                            entity.CreatedDate = DateTime.UtcNow;
+                            entity.UpdatedDate = DateTime.UtcNow;
+                            break;
+                        }
+                    case EntityState.Modified:
+                    case EntityState.Deleted:
+                        {
+                            entity.UpdatedDate = DateTime.UtcNow;
+                            break;
+                        }
+                }
+            }
+        }
     }
 }
